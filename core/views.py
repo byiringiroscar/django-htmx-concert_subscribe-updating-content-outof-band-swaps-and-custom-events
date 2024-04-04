549f88d37ddffb7a5ae2d50b2fadcec7420fb618
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-04 11:41:11 +0200

    Add sample concert

--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from core.models import Event
 
 # Create your views here.
 def event_detail(request, pk):
-    context = {}
+    event = get_object_or_404(Event, pk=pk)
+    print('-----------', event)
+    context = {
+        'event': event
+    }
     return render(request, 'core/subscribe.html', context)
