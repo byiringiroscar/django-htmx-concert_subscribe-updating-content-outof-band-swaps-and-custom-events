256682d4b45eeea22b68e803fc9d007ebd692cb4
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-04 11:49:54 +0200

    wip

--- core/views.py
+++ core/views.py
@@ -4,7 +4,6 @@ from core.models import Event
 # Create your views here.
 def event_detail(request, pk):
     event = get_object_or_404(Event, pk=pk)
-    print('-----------', event)
     context = {
         'event': event
     }
