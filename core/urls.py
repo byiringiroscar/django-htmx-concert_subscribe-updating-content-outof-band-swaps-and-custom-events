e9b53b981f8e5327f6e54e1c88eabdf9d56b9e89
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-04 12:31:50 +0200

    Add subscribe

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('event/<int:pk>/', views.event_detail, name='event-detail')
+    path('event/<int:pk>/', views.event_detail, name='event-detail'),
+    path('event/<int:pk>/subscribe', views.subscribe, name='subscribe'),
 ]
--- core/views.py
+++ core/views.py
@@ -8,3 +8,12 @@ def event_detail(request, pk):
         'event': event
     }
     return render(request, 'core/subscribe.html', context)
+
+
+def subscribe(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    event.users.add(request.user)
+    context = {
+        'event': event
+    }
+    return render(request, 'core/partials/user_li.html', context)
