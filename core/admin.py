65ab9a58ec35d763dad5da3154d7d78dc20f586a
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-04 11:25:45 +0200

    wip on event

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models import Event
 
 # Register your models here.
+
+
+admin.site.register(Event)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    users = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.name
